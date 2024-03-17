5dec91c1fad8727540f18e3cc1357ce8998f333d
Julia <juliamartins.td2.senai@gmail.com>
2024-03-16 22:03:50 -0300

    primiro commit declarando arrays

---
+++ atividadearrays.js
@@ -0,0 +1,13 @@
+//Aula array de javascrip!
+
+//Declaro variavel numA para primeiro array
+let numA = [5, 8, 3, 4, 7, 9, 7, 1, 2];
+
+//Declaro variavel numB para segundo array
+let numB = [1, 2, 6, 9, 5, 4, 7, 9];
+
+console.log("--- exibir Array A ---");
+console.log(numA);
+
+console.log("--- exibir Array B ---");
+console.log(numB);
